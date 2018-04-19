c3f86dd43ec85fbc56ff7cf6ae36e610640c768d
Manuel Escobar III <manuel.esco.20@gmail.com>
2018-04-19 08:38:06 -0600

    creating class and mutator's

---
+++ index.php
@@ -0,0 +1,18 @@
+
+class Car {
+	public $color
+	public $type
+}
+
+$chevy = newCar ("red","truck");
+$toyota = newCar ("blue","sport utility");
+
+
+class Car {
+public $numMiles;
+
+public function getNumMiles(): int {
+return($this->numMiles);
+}
+}
+
